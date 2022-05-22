457315113e447caa034824514dc35ed02c4f2633
Karmelova <kamila.weronika.marcinek@gmail.com>
2022-05-22 18:22:31 +0200

    refactoring project

---
+++ src/js/scripts.js
@@ -0,0 +1,5 @@
+$( 'input[class*=value]' ).on( 'keyup', function() {
+    if( $( this ).val().length !== 11 ) {
+    if( !/^\d{11}$/.test( $( this ).val() ) ) {
+        PatientData.hideCWUbDataInformation();
+        return;
