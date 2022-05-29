596013cf701c02d3fbc6a02a4bfbdcc6638f6028
Karmelova <kamila.weronika.marcinek@gmail.com>
2022-05-29 03:10:50 +0200

    add logic mov

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,3 +7,38 @@ document.querySelectorAll('.hex').forEach(item => {
         }
     })
 })
+
+function mov() {
+    document.querySelector('#mov1').style.border = 'none';
+    document.querySelector('#mov2').style.border = 'none';
+
+    const list1 = document.querySelector('#mov1').value;
+    const list2 = document.querySelector('#mov2').value;
+    let value1 = getRegister(list1);
+    let value2 = getRegister(list2);
+
+    if (value1 == null) {
+        document.querySelector('#mov1').style.border = 'solid red 1px';
+    }
+    if (value2 == null) {
+        document.querySelector('#mov2').style.border = 'solid red 1px';
+    }
+
+    
+}
+
+function getRegister(event) {
+    switch (event) {
+        case '1':
+            return document.querySelector('#ax').value;
+        case '2':
+            return document.querySelector('#bx').value;
+        case '3':
+            return document.querySelector('#cx').value;
+        case '4':
+            return document.querySelector('#dx').value;
+        default:
+            return null;
+
+    }
+}
